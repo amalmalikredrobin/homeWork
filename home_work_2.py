412de638ffff301f28e357a78e804f1075c662bc
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-11-13 15:24:37 +0300

    first commit

---
+++ home_work_2.py
@@ -0,0 +1,57 @@
+#1
+# a = int(input("Vvedite nomer: "))
+# if 0 == a : print("Verno")
+# else: print("Neverno")
+
+#2
+# a = int(input("Vvedite nomer: "))
+# if 0 < a : print("Verno")
+# else: print("Neverno")
+
+#3
+# a = int(input("Vvedite nomer: "))
+# if 0 > a : print("Verno")
+# else: print("Neverno")
+
+#4
+# a = int(input("Vvedite nomer: "))
+# if 0 < a : print("Verno")
+# elif 0 == a : print("Verno")
+# else: print("Neverno")
+
+#5
+# a = int(input("Vvedite nomer: "))
+# if 0 > a : print("Verno")
+# elif 0 == a : print("Verno")
+# else: print("Neverno")
+
+#6
+# a = int(input("Vvedite nomer: "))
+# if 0 != a : print("Verno")
+# else: print("Neverno")
+
+#7
+# a = str(input("Vvedite nomer: "))
+# if "test" == a : print("Verno")
+# else: print("Neverno")
+
+#8
+# a = int(str(input("Vvedite nomer: ")))
+# if '1' == a : print("Verno")
+# elif 1 == a : print("Verno")
+# else: print("Neverno")
+
+#9. Пользователь вводит порядковый номер пальца руки.
+#  Необходимо показать его название на экран.
+# a = int(input("Inter Finger Number: "))
+# if 1 == a: print("First Finger")
+# elif 2 == a: print("Second Finger")
+# elif 3 == a: print("Third Finger")
+# elif 4 == a: print("Fourth Finger")
+# elif 5 == a: print("Fifth Finger")
+# elif 6 == a: print("Sixth Finger")
+# elif 7 == a: print("Seventh Finger")
+# elif 8 == a: print("Eighth Finger")
+# elif 9 == a: print("Ninth Finger")
+# elif 10 == a: print("Tenth Finger")
+# else : print("THERE IS NO SUCH A FINGER(")
